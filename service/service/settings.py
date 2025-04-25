a38d5c6a31aee8268bc2d7488a2331e4d669adc1
Marat Shaibakov <smaspb17@gmail.com>
2025-04-25 15:07:39 +0300

    Day 3 - 1

--- service/service/settings.py
+++ service/service/settings.py
@@ -117,3 +117,21 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# логирование в консоль
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
+
