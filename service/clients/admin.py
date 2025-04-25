e16964c992d90beaa259dd76cdbb6114a5a6bf1b
Marat Shaibakov <smaspb17@gmail.com>
2025-04-25 13:45:41 +0300

    Day 2 - 1

---
+++ service/clients/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from clients.models import Client
+
+admin.site.register(Client)
+
+
+
---
+++ service/services/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from services.models import Service, Subscription, Plan
+
+
+admin.site.register(Service)
+admin.site.register(Plan)
+admin.site.register(Subscription)
+
+
